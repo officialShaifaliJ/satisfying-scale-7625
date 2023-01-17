7b0d4311644ad994b094d61960dbf6ee13d27e36
shaifali <shaifali02000@gmail.com>
2023-01-17 17:33:28 +0530

    commit_1_d_1

---
+++ blue_apron/src/components/AllRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Routes,Route} from 'react-router-dom';
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+export default function AllRoutes(){
+ return <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+ </Routes>
+}
---
+++ blue_apron/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+ export default function Navbar(){
+    return <div>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+    </div>
+ }
