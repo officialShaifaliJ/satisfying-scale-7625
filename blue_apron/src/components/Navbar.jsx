620a9ab448906a073b71eb7f8bbb24755080a0db
shaifali <shaifali02000@gmail.com>
2023-01-17 18:24:38 +0530

    commit1_day-2

--- blue_apron/src/components/Navbar.jsx
+++ blue_apron/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
  export default function Navbar(){
     return <div>
         <Link to="/">Home</Link>
+        <Link to="/on-the-menu">On the menu</Link>
         <Link to="/login">Login</Link>
     </div>
  }
